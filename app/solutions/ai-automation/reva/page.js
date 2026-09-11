fc403a6f1069cd21ad29ef937ed81887d0ae48b9
testingonline143 <boysgauatm@gmail.com>
2026-09-11 18:38:15 +0530

    feat: add REVA route

---
+++ app/solutions/ai-automation/reva/page.js
@@ -0,0 +1,3 @@
+import PageRouter,{pageMetadata} from "../../../PageRouter";
+export function generateMetadata(){return pageMetadata("solution:reva")}
+export default function RevaPage(){return <PageRouter pageKey="solution:reva"/>}
