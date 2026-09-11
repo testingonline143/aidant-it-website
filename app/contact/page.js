7e5a6943caa75f3db6d3d3c95a4ce43cfad99238
testingonline143 <boysgauatm@gmail.com>
2026-09-11 18:38:33 +0530

    feat: add contact page

---
+++ app/contact/page.js
@@ -0,0 +1,3 @@
+import PageRouter,{pageMetadata} from "../PageRouter";
+export function generateMetadata(){return pageMetadata("contact")}
+export default function ContactPage(){return <PageRouter pageKey="contact"/>}
